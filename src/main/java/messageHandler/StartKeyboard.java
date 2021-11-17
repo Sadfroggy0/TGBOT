ca692783955fa6c438224f117ead1fb4d84ab879
Тимофей Панов <m2005280@edu.misis.ru>
2021-11-17 07:48:43 +0300

    добавлены методы в DBController

--- src/main/java/messageHandler/StartKeyboard.java
+++ src/main/java/messageHandler/StartKeyboard.java
@@ -1,5 +1,6 @@
 package messageHandler;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import java.util.ArrayList;
