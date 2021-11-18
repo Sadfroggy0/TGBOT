7119984b6398a154098048aba716b5893e42f97a
Тимофей Панов <m2005280@edu.misis.ru>
2021-11-18 13:12:48 +0300

    Auto-Mailing Release

--- src/main/java/mailing/Mailing.java
+++ src/main/java/mailing/Mailing.java
@@ -3,32 +3,15 @@ package mailing;
 import dbHandler.DBController;
 import messageHandler.MessageHandler;
 import rssParser.News;
-import rssParser.RssParser;
+
 
 import java.util.ArrayList;
-import java.util.TimerTask;
 
-public class Mailing extends TimerTask {
+
+public class Mailing  {
     public static ArrayList<String>subs = new ArrayList<>();
     public static ArrayList<News> mailingNews= new ArrayList<>();
 
     private String id;
 
-    @Override
-    public void run() {
-        for (int i =0;i<News.cnbcLinks.length;i++) {
-            try {
-                new RssParser(News.cnbcLinks[i]);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            for (int j =1;j<News.newsList.size();j++){
-                DBController.saveMailingNews(News.newsList.get(j),i);
-            }
-            News.newsList.clear();
-        }
-        MessageHandler messageHandler = new MessageHandler();
-        messageHandler.mailingExecution();
-
-    }
 }
